0c5c4b336e738aa96e95b96b30536bfaa284e715
bioa10 <techguy1997@yahoo.com>
2023-05-11 13:12:32 -0500

    Set up pycharm project files and set up the project for better CI/CD

---
+++ emberwind/emberwind.py
@@ -0,0 +1,3 @@
+class Client:
+    def __init__(self, email: str, password: str):
+        pass
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["aiohttp>=3"]
+
+setup(
+    name="emberwind.py",
+    version="0.0.1",
+    author="MimicByte",
+    author_email="Aidan.MurphyCM@gmail.com",
+    description="A Python wrapper for the Emberwind API",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nomnivore-Games/emberwind.py",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
